b2a6d3b94fef6ccc0741a95bb77adbd4da2b3814
VISHESH  GATHA <101997238+vishesh9096@users.noreply.github.com>
2023-07-11 23:34:34 +0530

    initial

---
+++ hello/hello/hello.h
@@ -0,0 +1,18 @@
+//
+//  hello.h
+//  hello
+//
+//  Created by Vishesh Gatha on 02/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for hello.
+FOUNDATION_EXPORT double helloVersionNumber;
+
+//! Project version string for hello.
+FOUNDATION_EXPORT const unsigned char helloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hello/PublicHeader.h>
+
+
